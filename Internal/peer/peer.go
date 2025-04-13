a7e2e7508d7c53164f87390394dec922f9e21569
Jhelani Stokes <jhelani.stokes@gmail.com>
2025-04-12 20:04:57 -0400

    started writing peer logic, currently wip

---
+++ Internal/peer/peer.go
@@ -0,0 +1,59 @@
+package peer
+
+import (
+	"bufio"
+	"context"
+	"crypto/tls"
+	"fmt"
+	"github.com/quic-go/quic-go"
+	"strconv"
+	"strings"
+)
+
+func StartServer(port int) error {
+	tlsConfig := &tls.Config{
+		InsecureSkipVerify: true,
+		NextProtos:         []string{"quic-p2p"},
+	}
+	listener, err := quic.ListenAddr("localhost:"+strconv.Itoa(port), tlsConfig, nil)
+	if err != nil {
+		return err
+	}
+	conn, err := listener.Accept(context.Background())
+	if err != nil {
+		return err
+	}
+	stream, err := conn.AcceptStream(context.Background())
+	if err != nil {
+		return err
+	}
+	err = HandleStream(stream)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func Connect(addr string) error {
+	tlsConfig := &tls.Config{
+		InsecureSkipVerify: true,
+		NextProtos:         []string{"quic-p2p"},
+	}
+	conn, err := quic.DialAddr(context.Background(), addr, tlsConfig, nil)
+	if err != nil {
+		return err
+	}
+	stream, err := conn.OpenStreamSync(context.Background())
+}
+
+func HandleStream(stream quic.Stream) error {
+	reader := bufio.NewReader(stream)
+	line, err := reader.ReadString('\n')
+	if err != nil {
+		return err
+	}
+	cmd := strings.TrimSpace(line)
+
+	fmt.Println(cmd)
+	return nil
+}
