a699ef9db36eab1b5c9e1188733ecbec9cfe6fa5
Jhelani Stokes <jhelani.stokes@gmail.com>
2025-04-17 22:52:40 -0400

    Added ProvidedFiles.json, a list of filemetadatas that a peer has available to share. adjusted SaveMetadata and LoadMetadata functions accordingly

--- Internal/chunker/metadata.go
+++ Internal/chunker/metadata.go
@@ -2,6 +2,7 @@ package chunker
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
 )
 
@@ -19,27 +20,80 @@ type FileMetaData struct {
 	Chunks     []ChunkMetaData
 }
 
-func SaveMetaData(fileMeta *FileMetaData, outPath string) error {
-	file, err := os.Create(outPath)
+type ProvidedFiles struct {
+	Files []FileMetaData
+}
+
+type DataNotFound struct {
+	Reason string
+}
+
+func (e DataNotFound) Error() string {
+	return fmt.Sprintf("%s Not Found", e.Reason)
+}
+
+func SaveMetaData(fileMeta *FileMetaData) error {
+	var file *os.File
+	var err error
+	_, err = os.Stat("ProvidedFiles")
+	if os.IsNotExist(err) {
+		file, err = os.Create("ProvidedFiles")
+		if err != nil {
+			return err
+		}
+	} else {
+		file, err = os.Open("ProvidedFiles")
+		if err != nil {
+			return err
+		}
+	}
+	defer file.Close()
+
+	var providedFiles ProvidedFiles
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&providedFiles)
 	if err != nil {
 		return err
 	}
-	defer file.Close()
+
+	providedFiles.Files = append(providedFiles.Files, *fileMeta)
 
 	encoder := json.NewEncoder(file)
 	encoder.SetIndent("", "  ")
-	err = encoder.Encode(fileMeta)
+	err = encoder.Encode(providedFiles)
 	return err
 }
 
-func LoadMetaData(path string) (*FileMetaData, error) {
-	file, err := os.Open(path)
+func LoadMetaData(name string) (*FileMetaData, error) {
+	var file *os.File
+	var err error
+	var providedFiles ProvidedFiles
+	_, err = os.Stat("ProvidedFiles")
+	if os.IsNotExist(err) {
+		return nil, DataNotFound{Reason: name}
+	} else {
+		file, err = os.Open("ProvidedFiles")
+		if err != nil {
+			return nil, err
+		}
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&providedFiles)
 	if err != nil {
 		return nil, err
 	}
-	defer file.Close()
-	var fileMeta FileMetaData
-	decoder := json.NewDecoder(file)
-	err = decoder.Decode(&fileMeta)
-	return &fileMeta, err
+
+	if providedFiles.Files == nil {
+		return nil, DataNotFound{Reason: name}
+	}
+
+	for _, fileMeta := range providedFiles.Files {
+		if fileMeta.Name == name {
+			return &fileMeta, nil
+		}
+	}
+
+	return nil, DataNotFound{Reason: name}
 }
